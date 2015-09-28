18daf5afd4205b6a409920f9b91ed88b152a464c
ignaciolebrero <ignaciolebrero@gmail.com>
2015-09-28 13:43:18 -0300

    nuevo BFS con pesos ya recorridos

--- src/tp2/exercises/VerticeAdyacente.java
+++ src/tp2/exercises/VerticeAdyacente.java
@@ -1,12 +1,14 @@
 package tp2.Excercise2;
 
 public class VerticeAdyacente {
-    public int nodo;
-    public int peso;
+    private int nodo;
+    private int peso;
+    private int pesoYaRecorrido
     
     public VerticeAdyacente(int nodo, int peso) {
         this.nodo = nodo;
         this.peso = peso;
+        this.pesoYaRecorrido = Integer.MAX_VALUE;
     }
 
     public int getNodo() {
@@ -16,4 +18,12 @@ public class VerticeAdyacente {
 	public int getPeso() {
 		return peso;
 	}
+
+    public int getPesoYaRecorrido() {
+        return pesoYaRecorrido;
+    }
+
+    public void setPesoYaRecorrido(int peso) {
+        return this.pesoYaRecorrido = peso;
+    }
 }
