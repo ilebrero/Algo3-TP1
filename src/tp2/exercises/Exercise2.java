f4d7eeba2e4cd8341932ecb29db06b0d8123f61b
Kevin Fuksman <kevinfuksman@MacBook-Pro-de-Kevin.local>
2015-09-28 16:28:49 -0300

    empezando el 2

--- src/tp2/exercises/Exercise2.java
+++ src/tp2/exercises/Exercise2.java
@@ -1,17 +1,38 @@
 package tp2.exercises;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise2 {
-
-	public Exercise2(int parseInt, int parseInt2,
-			List<Portal<Baldoza>> parsePortales) {
-		// TODO Auto-generated constructor stub
+	private int pisoFin;
+	private int baldozaFin;
+	private List<List<Portal<Baldoza>>> baldozas;
+	private Portal<Baldoza> inicio;
+	public Exercise2(int pisoFin, int baldozaFin,List<Portal<Baldoza>> portales) {
+		this.pisoFin = pisoFin;
+		this.baldozaFin = baldozaFin;
+		this.inicio = portales.get(0);
+		
+		baldozas = new ArrayList<List<Portal<Baldoza>>>();
+		for (int i = 0; i < pisoFin; i++) {
+			List<Portal<Baldoza>> bal = new ArrayList<Portal<Baldoza>>();
+			baldozas.add(bal);
+		}
+		
+		for (int i = 0; i < portales.size(); i++) {
+			Portal<Baldoza> portal = portales.get(i);
+			baldozas.get(portal.getDesde().getPiso()).add(portal);
+		}
+		
 	}
 
-	public Object solve() {
-		// TODO Auto-generated method stub
-		return null;
+	public int solve() {
+		return DFS(this.inicio);
+	}
+
+	private int DFS(Portal<Baldoza> inicio) {
+		
+		return 0;
 	}
 
 }
